5429f44681cf343968f89ba25be8cfe50d4d6b07
XCR1793 <138337647+XCR1793@users.noreply.github.com>
2024-12-13 12:56:26 +1100

    All keys now fully configurable with any standard keyboard key combination

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -2,6 +2,7 @@
 #include <FastLED.h>
 #include <USB.h>
 #include <USBHIDKeyboard.h>
+#include <USBHIDMouse.h>
 
 #define led_pin 35
 #define led_count 21
@@ -20,6 +21,9 @@ int pins[] = {1, 4, 5, 38, 37, 48, 47, 21, 8, 7, 6, 9, 18, 17, 16, 15, 10, 11, 1
 // Create USB HID Keyboard instance
 USBHIDKeyboard keyboard;
 
+// Create USB HID Mouse instance
+USBHIDMouse mouse;
+
 /** #################################
  *  #                               #
  *  #     Key Macro Assignments     #
@@ -28,10 +32,21 @@ USBHIDKeyboard keyboard;
 */
 
 const char* KeyAssignments[NUM_PINS] = {
+  "shift",
+  "q",
+  "w",
+  "e",
+  "ctrl",
+  "right_click",
   "a",
-  "ctrl+t",
-  "ctrl+c",
-  "ctrl+v"
+  "s",
+  "d",
+  "",
+  "left_click",
+  "ctrl",
+  "esc",
+  "c",
+  " "
 };
 
 /** #################################
@@ -47,14 +62,26 @@ void sendKeyCombination(const char* combination) {
   String keys[3]; // Max of 3 keys in a combination
   int keyCount = 0;
 
-  // Parse combination string by '+'
+  // Check for mouse actions first
+  if (combStr == "left_click") {
+    mouse.click(MOUSE_LEFT);
+    return;
+  } else if (combStr == "right_click") {
+    mouse.click(MOUSE_RIGHT);
+    return;
+  } else if (combStr == "middle_click") {
+    mouse.click(MOUSE_MIDDLE);
+    return;
+  }
+
+  // Parse keyboard combination string by '+'
   while ((idx = combStr.indexOf('+', start)) != -1 && keyCount < 3) {
     keys[keyCount++] = combStr.substring(start, idx);
     start = idx + 1;
   }
   keys[keyCount++] = combStr.substring(start);
 
-  // Send keys
+  // Send keyboard keys
   for (int i = 0; i < keyCount; i++) {
     if (keys[i] == "ctrl") {
       keyboard.press(KEY_LEFT_CTRL);
@@ -62,11 +89,41 @@ void sendKeyCombination(const char* combination) {
       keyboard.press(KEY_LEFT_ALT);
     } else if (keys[i] == "shift") {
       keyboard.press(KEY_LEFT_SHIFT);
+    } else if (keys[i] == "esc") {
+      keyboard.press(KEY_ESC);
     } else if (keys[i].length() == 1) { // Single character key
       keyboard.press(keys[i][0]);
     }
   }
 }
+
+void releaseKeyCombination(const char* combination) {
+  String combStr = String(combination);
+  int start = 0, idx;
+  String keys[3];
+  int keyCount = 0;
+
+  while ((idx = combStr.indexOf('+', start)) != -1 && keyCount < 3) {
+    keys[keyCount++] = combStr.substring(start, idx);
+    start = idx + 1;
+  }
+  keys[keyCount++] = combStr.substring(start);
+
+  for (int i = 0; i < keyCount; i++) {
+    if (keys[i] == "ctrl") {
+      keyboard.release(KEY_LEFT_CTRL);
+    } else if (keys[i] == "alt") {
+      keyboard.release(KEY_LEFT_ALT);
+    } else if (keys[i] == "shift") {
+      keyboard.release(KEY_LEFT_SHIFT);
+    } else if (keys[i] == "esc") {
+      keyboard.release(KEY_ESC);
+    } else if (keys[i].length() == 1) {
+      keyboard.release(keys[i][0]);
+    }
+  }
+}
+
 
 // Pin Mapper
 int mapPinValue(int pin) {
@@ -75,7 +132,7 @@ int mapPinValue(int pin) {
     return 4 - pin;
   } else if (pin >= 10 && pin <= 14) {
     // Map 10 to 14 to 5 to 9
-    return pin - 5;
+    return 19 - pin;
   } else if (pin >= 5 && pin <= 9) {
     // Map 5 to 9 to 10 to 14
     return pin + 5;
@@ -87,7 +144,7 @@ int mapPinValue(int pin) {
 
 // Initialise Variables
 signed int internalDelay[NUM_PINS] = {};
-signed int pressdelay = 200;
+signed int pressdelay = 25; // ################# Synchronised Timing delay in ms #################
 
 void setup(){
   // Enable Serial
@@ -118,20 +175,23 @@ void loop(){
       pin--;
       // Read Potentiometer Values Here
     }else if(digitalRead(pins[i])){
-      if((pin <= sizeof(KeyAssignments)) && (internalDelay[i] <= 0)){
+      // if((pin <= sizeof(KeyAssignments)) && (internalDelay[i] <= 0)){ // Switch the if statment below with this one to enable synchronised timing
+      if(pin <= sizeof(KeyAssignments)){ // Switch this with above to enable synchronised timing
+        Serial.println(KeyAssignments[mapPinValue(pin)]);
         sendKeyCombination(KeyAssignments[mapPinValue(pin)]);
-        internalDelay[i] = pressdelay;
-        keyboard.releaseAll();
+        // internalDelay[i] = pressdelay; // Enable this to Allow the keys to set timers (REQUIRED FOR SYNCHRONISED TIMING)
       }
+    }else if(!digitalRead(pins[i])){
+      releaseKeyCombination(KeyAssignments[mapPinValue(pin)]);
     }
     pin++;
   }
-  for(size_t i = 0; i < NUM_PINS; i++){
-    internalDelay[i] = internalDelay[i] - 1;
-    if(internalDelay[i] <= 0){
-      internalDelay[i] = 0;
-    }
-  }
+  // for(size_t i = 0; i < NUM_PINS; i++){ // This is required to count the timer for Synchronised Timing
+  //   internalDelay[i] = internalDelay[i] - 1;
+  //   if(internalDelay[i] <= 0){
+  //     internalDelay[i] = 0;
+  //   }
+  // }
   delay(1);
 }
 
